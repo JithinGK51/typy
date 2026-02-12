3867f42d1ca2cd016465e7b5ed7f0c1944984448
JithinGK51 <jithingk51@users.noreply.github.com>
2026-02-12 10:11:04 +0530

    initial commit

---
+++ order.py
@@ -0,0 +1,6 @@
+#creating the order view and other things using py and using the input function to get the order details from the user and then storing it in a list and then displaying the order details to the user
+
+a = input("Enter the order details: ")
+order_list = []
+order_list.append(a)
+print("Order details: ", order_list)
