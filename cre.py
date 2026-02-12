507e37d7836e18235bccab381b678ff4f7ee4cf8
JithinGK51 <jithingk51@users.noreply.github.com>
2026-02-12 10:20:35 +0530

    added new file

---
+++ cre.py
@@ -0,0 +1,8 @@
+# student passing in exam or not
+
+a = int(input("Enter the marks obtained: "))
+if a >= 50:
+    print("Congratulations! You have passed the exam.")
+else:
+    print("Sorry, you have failed the exam. Better luck next time!")
+
